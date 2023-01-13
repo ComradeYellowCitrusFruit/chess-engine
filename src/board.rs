7e5309e1c629fc325bdc0009ec95d65b05083cc3
Teresa <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2023-01-13 12:35:15 -0500

    Wrote board.rs
    Wrote board.rs, a file containing structures and functions directly relating to boards and pieces.

---
+++ src/board.rs
@@ -0,0 +1,40 @@
+enum pieceType
+{
+	empty,
+	bPawn,
+	bKnight,
+	bBishop,
+	bRook,
+	bKing,
+	bQueen,
+	wPawn,
+	wKnight,
+	wBishop,
+	wRook,
+	wKing,
+	wQueen
+}
+
+impl pieceType
+{
+	fn centipawns(&self) -> u32
+	{
+		match self
+		{
+			pieceType.empty => return 0;
+			pieceType.bPawn | pieceType.wPawn => return 100;
+			pieceType.bKnight | pieceType.wKnight | pieceType.bBishop | pieceType.wBishop => return 300;
+			pieceType.bRook | pieceType.wRook => return 500;
+			pieceType.bQueen | pieceType.wQueen => return 900;
+			pieceType.bKing | pieceType.wKing => return 1_000_000_000;
+		}
+	}
+}
+
+struct board
+{
+	bCastled: mut bool;
+	wCastled: mut bool;
+	toMove: bool;
+	pieces: [mut [mut piece; 8]; 8];
+}
