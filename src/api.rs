8ed922a5631f64fdef4f20fb1ae8a652450b6523
Teresa Fuckyouiamnotputingmylastnameheresuckmycock <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2023-01-18 21:48:21 -0500

    Wrote a CXX bridge for the api

--- src/api.rs
+++ src/api.rs
@@ -19,4 +19,29 @@ pub fn getCheckmateStatus(FEN: &str) -> u8;
 */
 pub fn isLegal(FEN: &str, AN: &str) -> bool;
 
-// TODO: Add a cxx bridge
+#[cxx::bridge]
+mod ffi
+{
+    #[namespace = "chess"]
+    extern "Rust"
+    {
+        /*  Generate next move
+        *   @param FEN The FEN string to use to generate the next move from
+        *   @param depth How many moves to think ahead, less means a dumber AI, faster compuation, and less memory used
+        */
+        pub fn generateMove(FEN: &str, depth: i16) -> CxxString;
+
+        /*  Get checkmate status, as a set of bitflags
+        *   bit 0 - Set if white is in checkmate
+        *   bit 1 - Set if black is in checkmate
+        *   bit 2 - Set if position is illegal
+        */
+        pub fn getCheckmateStatus(FEN: &str) -> u8;
+
+        /*  Is a move legal?
+        *   @param FEN The FEN string to use to check legality
+        *   @param AN The algebraic notation form of the move
+        */
+        pub fn isLegal(FEN: &str, AN: &str) -> bool;
+    }
+}
